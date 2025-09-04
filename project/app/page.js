247774d298af547bdc4a67dcbca973bcb86681dc
Harman sahota <sahotaharman59111@gmail.com>
2025-09-04 07:07:23 +0530

    todo

--- project/app/page.js
+++ project/app/page.js
@@ -1,8 +1,34 @@
-import React from 'react'
+'use client'
+import axios from 'axios'
+import React, { useEffect, useState } from 'react'
+
 
 const page = () => {
+  
+const [todo, settodo] = useState([])
+const [title, settitle] = useState('')
+
+useEffect(()=>{
+  axios.get('/api/todos').then((res)=>{
+    settodo(res.data)
+  })
+},[])
+
+async function addTodo() {
+const res =  await axios.post('/api/todos',{title})
+settodo([...todo , res.data])
+settitle("")
+}
   return (
-    <div>page</div>
+    <div>
+      <h1>TODOS</h1>
+      <input 
+      placeholder='enter title'
+      value={title}
+      onChange={(e)=>settitle(e.target.value)}
+      />
+      <button onClick={addTodo}>Add </button>
+    </div>
   )
 }
 
